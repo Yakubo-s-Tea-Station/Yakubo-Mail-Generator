d8226ce92dedb16488ecec7eb90f7bc854fa2377
与田祐希是真的天下第一 <walter_d@qq.com>
2021-01-22 07:08:56 +0800

    Init

---
+++ js/index.js
@@ -0,0 +1,146 @@
+let state;
+if (window.localStorage) {
+    state = JSON.parse(window.localStorage.getItem("state"));
+    if (window.location.search) {
+        let params = (new URL(document.location)).searchParams;
+        if (params.get("resetState") != null) {
+            state = null;
+        }
+    }
+}
+
+$(document).ready(function(){
+
+    if (!state) {
+        state = {
+            messages: [], 
+            doc_expanded: true
+        };
+    }else{
+        loadMessages();
+    }
+});
+function loadMessages() {
+    if (state.messages && state.messages.length > 0) {
+        $(".messages-body>*").each(function () { $(this).remove(); });
+        for (const message of state.messages) {
+            if (message.type === "text") {
+                addText(message.value,message.left?"left":"right");
+            }
+            if (message.type === "image") {
+                addImage(message.img,message.wtr,message.left?"left":"right");
+            }
+            if (message.type === "datetime") {
+                addDatetime(message.value);
+            }
+        }
+    }
+}
+function saveMessages() {
+    state.messages = [];
+    $("div.messages-body > div").each(function () {
+        if ($(this).hasClass("time-budge")) {
+            state.messages.push({
+                type: "datetime",
+                value: $(this).text()
+            });
+        } else if ($(this).hasClass("text-block")) {
+            state.messages.push({
+                type: "text",
+                left: $(this).hasClass("left-block"),
+                value: $(this).text()
+            });
+        } else if ($(this).hasClass("image-block")) {
+            state.messages.push({
+                type: "image",
+                left: $(this).hasClass("left-block"),
+                img: $(this).find("img:nth-of-type(1)").attr("src"),
+                wtr: $(this).find("img:nth-of-type(2)").attr("src")
+            });
+        }
+    });
+    if (window.localStorage) {
+        window.localStorage.setItem("state", JSON.stringify(state));
+    }
+}
+function saveImage() {
+    $("body").addClass("body-lock");
+    document.body.scrollIntoView()
+    let htmlDom = document.querySelector('#messages-canvas')
+    html2canvas(htmlDom, {
+        allowTaint: false,
+        taintTest: true,
+        useCORS: true,
+        background: "#fff",
+    }).then(function (canvas) {
+        var link = document.createElement("a");
+        link.href = canvas.toDataURL('image/jpg');
+        link.download = 'screenshot.jpg';
+        link.click();
+    });
+    $("body").removeClass("body-lock");
+}
+function appendImageFromFile(file) {
+    var reader = new FileReader();
+    reader.addEventListener("load", function (event) {
+        addImage(event.target.result);
+    });
+    reader.readAsDataURL(file);
+}
+function addImage(path, path_wtr,side="left") {
+    $("div.messages-body").append("<div class=\"basic-block image-block "+side+"-block\"><span><i class=\"far fa-times-circle fa-2x i-red\"></i><i class=\"fas fa-retweet fa-2x i-green\"></i><img crossorigin=\"anonymous\" src=\"" + path + "\"><img crossorigin=\"anonymous\"></span><img src=\"素材/图层 2.png\"></div>");
+    saveMessages();
+}
+function addText(str,side="left") {
+    $("div.messages-body").append("<div class=\"basic-block text-block "+side+"-block\"><span><i class=\"far fa-times-circle fa-2x i-red\"></i><i class=\"fas fa-retweet fa-2x i-green\"></i><span contenteditable=\"true\">" + str + "</span></span><img src=\"素材/图层 2.png\"></div>");
+    saveMessages();
+}
+function addDatetime(val) {
+    $("div.messages-body").append("<div class=\"time-budge\"><span><i class=\"far fa-times-circle fa-2x i-red\"></i><span contenteditable=\"true\">" + val + "</span></span></div>");
+    saveMessages();
+}
+
+$(document).ready(function () {
+    $(document).on("load", "img", function (event) {
+        event.target.setAttribute("data-loaded", "true");
+    });
+    // 文字输入按钮click事件
+    $(document).on("click", "#enter-button", (function () {
+        let new_msg = $("#text-input").val().trim();
+        if (new_msg.length > 0) {
+            addText(new_msg);
+            $("#text-input").val("");
+        }
+        saveMessages();
+        $("#text-input")[0].scrollIntoView();
+        $("#text-input")[0].focus();
+    }));
+    // 拖拽载入图片事件
+    $("body").bind("drop", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        files = event.originalEvent.dataTransfer.files
+        if (files.length > 0) {
+            for (let i = 0; i < files.length; i++) {
+                appendImageFromFile(files[0]);
+            }
+        }
+    });
+    $("body").bind("dragover", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+
+    // 交换位置按钮
+    $(document).on("click", ".fa-retweet", function () {
+        block_root = $(this).parent().parent();
+        new_class = block_root.hasClass("left-block") ? "right-block" : "left-block";
+        block_root.removeClass("left-block").removeClass("right-block").addClass(new_class);
+        saveMessages();
+    });
+    // 删除按钮
+    $(document).on("click", ".fa-times-circle", function () {
+        $(this).parent().parent().remove();
+        saveMessages();
+    });
+});
