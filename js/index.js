55bf7a5878aa01f229a55bebc74cd2477ca95dd1
W.D <famous@no.com>
2021-03-12 19:32:18 +0800

    Update 浮动按钮系列完善

--- js/index.js
+++ js/index.js
@@ -24,20 +24,34 @@ function saveImage() {
 function saveProjectFile() {
 
 }
-function activateFloatingPanel(name){
+function activateFloatingPanel(name) {
     closeFloatingPanel();
-    $("#"+name+"-panel").removeClass("d-none");
+    $("#" + name + "-panel").removeClass("d-none");
 }
-function closeFloatingPanel(){
+function closeFloatingPanel() {
     $(".floating-panel:not(.d-none)").addClass("d-none");
     $(".floating-button").removeClass("floating-button-activated");
+}
+
+function getDateFromString(strDate) {
+    var date = eval('new Date(' + strDate.replace(/\d+(?=-[^-]+$)/,
+        function (a) { return parseInt(a, 10) - 1; }).match(/\d+/g) + ')');
+    return date;
 }
 
 $(() => {
     // 两侧按钮
     $(document).on("click", ".floating-button", (e) => {
+        let fn = $(e.target).attr("fbg-name");
+        if(fn=="update-logs"){
+            $(".new-info-point").addClass("d-none");
+            $.cookie("since-update-logs",new Date().toDateString(),{ expires: 3650 });
+        }
         activateFloatingPanel($(e.target).attr("fbg-name"));
         $(e.target).addClass("floating-button-activated");
+    });
+    $(document).on("click", ".floating-button-activated", (e) => {
+        closeFloatingPanel();
     });
     $(document).on("click", ".floating-panel-close-button", (e) => {
         closeFloatingPanel();
@@ -57,6 +71,22 @@ $(() => {
         dataType: "html",
         success: function (response) {
             $("#manuals-contents").html(response);
+        }
+    });
+    // 获取版本信息
+    $.ajax({
+        type:"GET",
+        url:"version-info.json",
+        dataType:"json",
+        success: function(response){
+            $(".version-number").text(response["version-number"]);
+            pd = getDateFromString(response["publish-date"]);
+            $(".publish-date").text(String(pd.getFullYear())+String(pd.getMonth()).padStart(2,'0')+String(pd.getDate()));
+            sul = $.cookie("since-update-logs");
+            console.log(sul);
+            if(!sul || pd>=getDateFromString($.cookie("since-update-logs"))){
+                $(".new-info-point").removeClass("d-none");
+            }
         }
     });
     loadData();
